6e12189ea4f937ac440f3b0b95918f917e7fb0de
ShahkarAhmed8 <shahkarahmedkhan8@gmail.com>
2025-01-08 10:40:23 +0500

    Add files via upload
    Codes  covered in first file
    ->data base
    ->operators
    ->type function and type casting
    ->#chapter :2 
    #string methods
    ->#chapter 4  lists and tuples
    ->#list methods
    -> tuple methods

---
+++ dictionary and sets.py
@@ -0,0 +1,32 @@
+m={
+   "harry": 100,
+   "akash": 70,
+   "nuno": 87   
+    
+   }
+#print(m,type(m))
+#print(m["nuno"])
+                      # dictionary methos
+#print(m.items())                     
+
+#print(m.keys())
+#print(m.get("nuno"))   #difference between m.get() and print(m["nuno"]) is .get returns none and the other returns
+                         
+
+#m.update({"nuno":88,"acb":81})
+print(m)
+
+
+#sets
+a={}      #this is empty dictionary not a set
+s=set()     #this is empty set
+
+
+
+
+
+
+
+
+
+                         
---
+++ first.py
@@ -0,0 +1,21 @@
+a=37
+c=89321
+v=345678
+print(a+c)
+
+#data base
+a=1                 # a is an integer
+b= 7.8              # b is a floating point number
+c="harry"           # c is a string
+d=True /False       # d is a boolean variable
+e=None              # e is none type variable
+
+
+#operators
+          #arthmatic 
+          #assignment
+          #comparison
+          #logical
+
+#type function and type casting
+          
